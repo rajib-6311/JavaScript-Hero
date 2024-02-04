aa80007f6f3dacdcc4068a98e11b3c1911b1cb75
rajibhossen <mhossen6311@gmail.com>
2024-02-04 23:08:37 +0600

    task one solve

--- task-1.js
+++ task-1.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+var oranges = 500;
+var apples = 200;
+var totalTk = 1000;
+console.log("total taka", totalTk);
+
+var totalBuy = oranges + apples;
+console.log("total buy :", totalBuy);
+
+var returnTk = totalTk - totalBuy;
+
+console.log("Shop kipper return :",returnTk )
