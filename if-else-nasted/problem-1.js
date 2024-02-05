4ca7559663600eb998fa1c87a210abb1bbad257a
rajibhossen <mhossen6311@gmail.com>
2024-02-05 16:14:53 +0600

    nasted if else problem

--- if-else-nasted/problem-1.js
+++ if-else-nasted/problem-1.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const money = 0;
+if (money > 300) {
+    console.log("You are rich!!");
+}
+else {
+    if (money > 200) {
+        console.log("You can buy a fuskka!!");       
+    }
+    else {
+        if (money > 0) {
+            console.log("You can buy bannana!!");
+        }
+        else {
+            console.log("YOU are poor!!!");
+        }
+    }
+}
