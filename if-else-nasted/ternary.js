977182f53bcf7db741c714032fd367724ab08f28
rajibhossen <mhossen6311@gmail.com>
2024-02-05 16:51:44 +0600

    ternary problem

---
+++ if-else-nasted/ternary.js
@@ -0,0 +1,36 @@
+// simple if else
+
+const age = 18;
+if (age >= 18) {
+    console.log("YOU can vote!!");
+}
+else {
+    console.log("You cannot vote");
+}
+
+
+/*simple ternary problem
+condition ? true : false
+*/
+// const age = 18;
+// age >= 18 ? console.log("You can vote") : console.log("You cannot vote");
+
+
+/* simple if else */
+
+// let price = 500;
+// const isLeader = false;
+// if (isLeader === true) {
+//     price = 0;
+// }
+// else {
+//     price += 100;
+// }
+// console.log(price);
+
+/* ternary if else */
+// let price = 500;
+// price = isLeader === false ? 0 : price + 100;
+// console.log(price);
+
+/* simple if else */
